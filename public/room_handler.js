fb856f28f21feac9beb0acf7609fdd9598c88b37
Dhruv Krishnamachari <dhruv.rajeshk@gmail.com>
2021-10-04 02:11:07 +0530

    Update room_handler.js

--- public/room_handler.js
+++ public/room_handler.js
@@ -2,7 +2,7 @@ const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
 const myPeer = new Peer(undefined, {
   secure: true,
-  host: "https://stella-meet.herokuapp.com/",
+  host: "stella-meet.herokuapp.com/",
   port: "3001",
 });
 
